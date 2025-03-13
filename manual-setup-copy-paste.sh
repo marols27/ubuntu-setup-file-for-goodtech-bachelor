4afa6681e6dacd3f360507fad8eda92a14f7206f
Martin Repvik Olsbø <43169864+marols27@users.noreply.github.com>
2025-03-13 15:59:44 +0100

    Update manual-setup-copy-paste.sh

--- manual-setup-copy-paste.sh
+++ manual-setup-copy-paste.sh
@@ -5,9 +5,9 @@ set -x
 echo "seting up locales..."
 locale
 sudo apt update
-sudo apt install locales
-locale-gen en_US en_US.UTF-8
-update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+sudo apt install locales -y
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale
 sudo apt autoremove -y
@@ -18,16 +18,15 @@ echo "locales setup done"
 echo "Starting ROS2 setup..."
 sudo apt install software-properties-common -y
 sudo add-apt-repository universe -y
-sudo apt update 
-sudo apt install curl gnupg2 lsb-release -y
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt install curl gnupg2 lsb-release -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update 
 sudo apt install ros-dev-tools -y
 sudo apt update
 sudo apt upgrade -y
 sudo apt install ros-jazzy-desktop -y
-sudo apt install ros-jazzy-ros-base -y
 echo "ROS2 setup done"
 
 
